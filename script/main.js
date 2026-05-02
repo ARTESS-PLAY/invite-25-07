9d4d6e9510894ca3e9f8981acc39eda931fdb69d
pan0mera <artess.ponomarev@mail.ru>
2026-05-02 12:53:40 +0300

    f

--- script/main.js
+++ script/main.js
@@ -122,35 +122,35 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const audio = new Audio('../audio.mp3');
-    audio.autoplay = true;
-    audio.loop = true;
-    audio.volume = 0.2;
+// document.addEventListener('DOMContentLoaded', () => {
+//     const audio = new Audio('../audio.mp3');
+//     audio.autoplay = true;
+//     audio.loop = true;
+//     audio.volume = 0.2;
 
-    const btn = document.getElementById('btn-music');
+//     const btn = document.getElementById('btn-music');
 
-    const playAudio = () => {
-        audio.play().catch((error) => console.log('Автовоспроизведение заблокировано: ', error));
-        document.body.removeEventListener('click', playAudio);
-        btn.classList.remove('play');
-        btn.classList.add('pause');
-    };
+//     const playAudio = () => {
+//         audio.play().catch((error) => console.log('Автовоспроизведение заблокировано: ', error));
+//         document.body.removeEventListener('click', playAudio);
+//         btn.classList.remove('play');
+//         btn.classList.add('pause');
+//     };
 
-    const pauseAudio = () => {
-        audio.pause();
-        btn.classList.add('play');
-        btn.classList.remove('pause');
-    };
+//     const pauseAudio = () => {
+//         audio.pause();
+//         btn.classList.add('play');
+//         btn.classList.remove('pause');
+//     };
 
-    btn.addEventListener('click', function () {
-        if (btn.classList.contains('play')) {
-            playAudio();
-        } else {
-            pauseAudio();
-        }
-    });
+//     btn.addEventListener('click', function () {
+//         if (btn.classList.contains('play')) {
+//             playAudio();
+//         } else {
+//             pauseAudio();
+//         }
+//     });
 
-    // Попытка воспроизведения с пользовательским взаимодействием
-    document.body.addEventListener('click', playAudio, { once: true });
-});
+//     // Попытка воспроизведения с пользовательским взаимодействием
+//     document.body.addEventListener('click', playAudio, { once: true });
+// });
